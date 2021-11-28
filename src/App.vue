05345910320f1ec7b58446032419b274593dd497
alamgirweb11 <alamgirweb11@gmail.com>
2021-11-29 01:15:28 +0600

    ui-design added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Weather msg="Welcome to vue Weather App."/>
+    <Weather/>
   </div>
 </template>
 
 <script>
-import Weather from './components/Weather.vue'
+import Weather from './components/Weather';
+
 
 export default {
   name: 'App',
   components: {
-    Weather
+             Weather
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+body{
+     font-family: 'montserrat', 'sans-serif';
+}
+
+
 </style>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,32 +1,124 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div id="wrapper">
+     <main>
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar form-control"
+          v-model="query"
+          placeholder="Search...">
+      </div>
+
+      <div class="weather-wrap">
+
+        <div class="location-box">
+          <div class="location">Bangladesh</div>
+          <div class="date">Monday 29 November 2021</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">8°C</div>
+          <div class="weather">Rain</div>
+        </div>
+
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Weather',
+  data(){
+     return{
+          api_key: 'b73d6cafea04aa33d16fc26d63447873',
+          base_url: 'https://api.openweathermap.org/data/2.5/',
+          query: '',
+          weather: {}
+     }
+  },
   props: {
-    msg: String
+    // msg: String
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+   #wrapper{
+        background-image: url('../assets/cloud-blue-sky.jpg');
+        background-size: cover;
+        background-position: bottom;
+        transition: 0.4s;
+   }
+   main{
+     min-height: 100vh;
+     padding: 25px;
+     background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+ .search-box{
+      width: 100%;
+      margin-bottom:  30px;
+ }
+ .search-box .search-bar{
+     display: block;
+     width: 100%;
+     padding: 15px;
+     columns: #3333;
+     font-size: 20px;
+     border: none;
+     appearance: none;
+     outline: none;
+     background: none;
+     background-color: rgba(255, 255, 255, 0.5);
+     border-radius: 0px 16px 0px 16px;
+     transition: 0.4s;
+ }
+
+ .search-box .search-bar:focus{
+      box-shadow: 0px 0px 16px rgba(255, 255, 255, 0.25);
+      background-color:  rgba(255, 255, 255, 0.75);
+      border-radius: 0px 16px 0px 16px;
+ }
+
+ /* weather info box styling */
+ .location-box .location{
+        color: #fff;
+        font-size: 35px;
+        font-weight: 500;
+        text-align: center;
+        text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+ }
+ .location-box .date{
+        color: #fff;
+        font-size: 24px;
+        font-weight: 300;
+        text-align: center;
+        font-style: italic;
+ }
+.weather-box{
+    text-align: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.weather-box .temp{
+      display: inline-block;
+      padding: 10px 25px;
+      color: #fff;
+      font-size: 108px;
+      font-weight: 900;
+      text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+      background-color: rgba(255, 255, 255, 0.25);
+      border-radius: 16px;
+      margin: 30px 0px;
+      box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
-a {
-  color: #42b983;
+
+.weather-box .weather{
+     color: #fff;
+        font-size: 44px;
+        font-weight: 700;
+        text-align: center;
+        font-style: italic; 
+        text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
